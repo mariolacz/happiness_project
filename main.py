fbb89d94c6266b7787f95ec056f75fea60fa1847
mariolacz <mczajkowska.work@gmail.com>
2025-05-26 21:18:51 +0200

    Added chart

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # Load data
-df = pd.read_csv('/Users/mariolaczajkowska/happiness_data/new_data/happiness_cleaned.csv')
+df = pd.read_csv('/Users/mariolaczajkowska/happiness_data/new_data/happiness_cleaned.csv', sep = ';')
 df['Year'] = df['Year'].astype(int)
 
 st.sidebar.header("Filters")
@@ -13,6 +13,21 @@ selected_year = st.sidebar.multiselect("Select Year:", options=sorted(df['Year']
 # Apply filters
 filtered_df = df[df['Country'].isin(selected_country) & df['Year'].isin(selected_year)]
 
-# Display data
-st.title("World Happiness Data")
-st.write("Filtered Data", filtered_df)
+tab1, tab2 = st.tabs(["Filtered Data", "Charts"])
+
+# Tab 1 – Data Table
+with tab1:
+    st.write("Filtered Data", filtered_df)
+
+with tab2:
+    st.title("Charts")
+
+    if 'Score' in filtered_df.columns:
+        #av score by country 
+        avg_scores = filtered_df.groupby("Country")["Score"].mean().reset_index()
+        fig_bar = px.bar(avg_scores, x="Country", y="Score", 
+                         title="Average happiness score by country",
+                         labels={"Score": "Average Score"})
+        st.plotly_chart(fig_bar)
+    else:
+        st.warning("No 'Score' column found in the dataset.")
